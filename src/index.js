01cbede9ddeea2ed2b556fab1fe439e83de51c4c
austenhasty <hastyae@gmail.com>
2020-03-31 17:26:30 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  const tasks = document.getElementById('tasks');
+  const newTaskDescription = document.getElementById('new-task-description');
+  const priority = document.getElementById('priority')
+
+  form.addEventListener('submit', addTask)
+
+
+  function addTask(event) {
+    event.preventDefault()
+    console.log("adding task")
+    const newTaskDescription = document.getElementById('new-task-description');
+    const newTask = document.createElement('li');
+    const deleteBtn = document.createElement('button')
+    newTask.innerText = newTaskDescription.value;
+    deleteBtn.innerText = 'X'
+    tasks.appendChild(newTask)
+    newTask.appendChild(deleteBtn)
+    setPriority(newTask)
+    resetValue()
+
+    deleteBtn.addEventListener('click', () => {
+      tasks.removeChild(newTask)
+    })
+  }
+
+  function setPriority(newTask) {
+    console.log(priority.value)
+    if (priority.value === 'High') {
+      newTask.style.color = 'red'
+    } else if (priority.value === 'Medium') {
+        newTask.style.color === 'yellow'
+    } else if (priority.value === 'Low') {
+        newTask.style.color === 'green'
+    } else {
+      window.alert('Please select a task priority level!')
+    }
+  }
+
+  function resetValue() {
+    newTaskDescription.value = ''
+  }
+
 });
